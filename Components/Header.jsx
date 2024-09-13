930f9bda5da6af7fccfe32b5db9c2c5f454bd679
AyushChauhan3757 <ayushc262@gmail.com>
2024-09-13 11:59:46 +0530

    Added Better Animations

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
             <div className='flex justify-between items-center'>
                 {/* --- Logo --- */}
                 <Image src={assets.logo} width={180} alt='' className='w-[130px] sm:w-auto'/>
-                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image src={assets.arrow}/> </button>
+                <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-solid border-black shadow-[-4px_4px_0px_#000000] hover:shadow-[-8px_8px_0px_#000000] hover:translate-y-[-2px] transition duration-300 ease-in-out active:shadow-[-4px_4px_0px_#000000] active:translate-y-[2px]'>Get Started <Image src={assets.arrow}/> </button>
             </div>
 
             <div className='text-center my-8'>
@@ -18,7 +18,7 @@ const Header = () => {
                 
                 <p className='mt-10 max-w-[740px] m-auto text-xs sm:text-base'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati praesentium repudiandae iusto recusandae odit molestias harum, natus alias sed provident velit rem quod!.</p>
 
-                <form className='flex justify-between max-w-[500px] scale-75 sm: scale-100 mx-auto mt-10 border border-black shadow-[-7px_7px_0px_#000000]'>
+                <form className='flex justify-between max-w-[500px] scale-75 sm: scale-100 mx-auto mt-10 border border-black shadow-[-4px_4px_0px_#000000] hover:shadow-[-8px_8px_0px_#000000] hover:translate-y-[-2px] transition duration-300 ease-in-out'>
 
                     <input type='email' placeholder='Enter Your Email' className='pl-4 outline-none '/>
 
