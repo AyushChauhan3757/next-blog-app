557900c7101ad7d3d18c5909b406b0436105e742
AyushChauhan3757 <ayushc262@gmail.com>
2024-09-13 11:21:33 +0530

    Improved Hover Effect

--- Components/BlogItems.jsx
+++ Components/BlogItems.jsx
@@ -1,27 +1,29 @@
-import { blog_data } from '@/Assets/assets';
+import { assets, blog_data } from '@/Assets/assets';
 import React from 'react';
 import Image from 'next/image';
 
-const BlogItems = () => {
+const BlogItems = ({ title, description, category, image }) => {
   return (
-    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000]'>
+    <div className='max-w-[330px] sm:max-w-[300px] bg-white border border-black hover:shadow-[-7px_7px_0px_#000000] hover:translate-y-[-2px] transition duration-300 ease-in-out'>
       {/* Image section */}
       <Image 
-        src={blog_data[0].image} 
-        alt={blog_data[0].title} 
+        src={image} 
+        alt='' 
         width={400} 
         height={400} 
         className='border-b border-black' 
       />
       
       {/* Category */}
-      <p className='ml-5 mt-5 px-2 inline-block bg-black text-white text-sm'>
-        {blog_data[0].category}
-      </p>
+      <p className='ml-5 mt-5 px-2 inline-block bg-black text-white text-sm'>{category}</p>
       
       {/* Content section */}
       <div className="p-5">
-        <h5 className='mb-2 text-lg font-medium'>{blog_data[0].title}</h5>
+        <h5 className='mb-2 text-lg font-medium'>{title}</h5>
+        <p className='mb-3 text-sm tracking-tight text-grey-700'>{description}</p>
+        <div className='inline-flex items-center py-2 font-semibolld text-center'>
+          Read More <Image src={assets.arrow} alt='' width={12} className='ml-2' />
+        </div>
       </div>
     </div>
   );
