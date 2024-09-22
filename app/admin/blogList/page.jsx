85dc3476e6271c355b880c4497f4f15cf2542d8d
Devraj choudhary <112477863+DevrajChdhary@users.noreply.github.com>
2024-09-22 14:19:50 +0530

    Api Completed Admin Blog List Started

--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -3,8 +3,18 @@ import React from 'react';
 
 const Page = () => {
     return (
-        <div>
-            Page
+        <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
+            <h1>All Blogs</h1>
+            <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'></div>
+            <table className='w-full text-sm text-gray-500'>
+                <thead className='text-sm text bg-gray-700 text-left uppercase bg-gray-50'>
+                    <tr>
+                        <th scope='col' className='hidden sm:block px-6 py-3'>
+                            Author Name
+                        </th>
+                    </tr>
+                </thead>
+            </table>
         </div>
     );
 };
