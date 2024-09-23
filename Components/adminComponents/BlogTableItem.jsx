1ba9e723a890335d94f66b6af901e05e6a4d95a2
AyushChauhan3757 <ayushc262@gmail.com>
2024-09-23 19:54:30 +0530

    Completed Blog List

--- Components/adminComponents/BlogTableItem.jsx
+++ Components/adminComponents/BlogTableItem.jsx
@@ -2,19 +2,23 @@ import { assets } from '@/Assets/assets';
 import Image from 'next/image';
 import React from 'react';
 
-const BlogTableItem = ({ authorImg, title }) => {
+const BlogTableItem = ({ authorImg, title, author, date }) => {
+
+    const blogDate = new Date(date);
+
     return (
         <tr className='bg-white border-b'>
             <th scope='row' className='items-center gap-3 hidden sm:flex px-6 py-4 font-medium text-gray-900 whitespace-nowrap'>
-                <Image src={ authorImg ? authorImg : assets.profile_icon } alt='' />
+                <Image width={40} height={40} src={ authorImg ? authorImg : assets.profile_icon } alt='' />
+                <p>{ author ? author : "No Author" }</p>
             </th>
             <td className='px-6 py-4'>
                 { title ? title : 'No Title'}
             </td>
             <td className='px-6 py-4'>
-                { "11 Jan 2024" }
+                { blogDate.toDateString() }
             </td>
-            <td className='px-6 py-4'>
+            <td className='px-10 py-4 cursor-pointer'>
                 X
             </td>
         </tr>
--- app/admin/blogList/page.jsx
+++ app/admin/blogList/page.jsx
@@ -1,31 +1,47 @@
 "use client";
 import BlogTableItem from '@/Components/adminComponents/BlogTableItem';
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 const Page = () => {
+
+    // --- Fetching Blogs ---
+    const [blogs, setBlogs] = useState([]);
+
+    const fetchBlogs = async () => {
+        const response = await axios.get('/api/blog');
+        setBlogs(response.data.blogs);
+    };
+    useEffect(() => {
+        fetchBlogs();
+    },[]);
+
     return (
         <div className='flex-1 pt-5 px-5 sm:pt-12 sm:pl-16'>
             <h1>All Blogs</h1>
             <div className='relative h-[80vh] max-w-[850px] overflow-x-auto mt-4 border border-gray-400 scrollbar-hide'>
                 <table className='w-full text-sm text-gray-500'>
-                    <thead className='text-sm text bg-gray-700 text-left uppercase bg-gray-50'>
+                    <thead className='text-sm text-gray-700 text-left uppercase bg-gray-50'>
                         <tr>
-                            <th scope='col' className='hidden sm:block px-6 py-3'>
+                            <th scope='col' className='px-6 py-3'>
                                 Author Name
                             </th>
-                            <th scope='col' className='hidden sm:block px-6 py-3'>
+                            <th scope='col' className='px-6 py-3'>
                                 Blog Title 
                             </th>
-                            <th scope='col' className='hidden sm:block px-6 py-3'>
-                                Blog Date
+                            <th scope='col' className='px-6 py-3'>
+                                Date
                             </th>
-                            <th scope='col' className='hidden sm:block px-6 py-3'>
+                            <th scope='col' className='px-6 py-3'>
                                 Action
                             </th>
                         </tr>
                     </thead>
                     <tbody>
-                        <BlogTableItem />
+                        {/* Fetching Blog List */}
+                        {blogs.map((item, index) => {
+                            return <BlogTableItem key={index} mongoId={item._id} title={item.title} author={item.author} authorImg={item.authorImg} date={item.date} />
+                        })}
                     </tbody>
                 </table>
             </div>
